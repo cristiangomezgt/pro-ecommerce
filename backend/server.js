825c06870ab1a3246f70f53433db48bb3348ea24
lumationDevelopers <67560096+cristiangomezgt@users.noreply.github.com>
2023-03-20 19:00:19 -0600

    ✨ Feat: Serve products

--- backend/server.js
+++ backend/server.js
@@ -6,4 +6,13 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+app.get('/api/products', (req, res) => {
+  res.json(products);
+});
+
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find((p) => p._id == req.params.id);
+  res.json(product);
+});
+
 app.listen(4000, console.log('Server running on port 5000'));
