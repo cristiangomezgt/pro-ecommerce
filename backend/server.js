c7ab9858e4285106a4ddfe90c206c75dfb1c0cfe
lumationDevelopers <67560096+cristiangomezgt@users.noreply.github.com>
2023-03-21 22:16:44 -0600

    🔧 Config: Set Nodemon & Concurrently

--- backend/server.js
+++ backend/server.js
@@ -15,4 +15,4 @@ app.get('/api/products/:id', (req, res) => {
   res.json(product);
 });
 
-app.listen(5000, console.log('Server running on port 5000'));
+app.listen(5000, console.log('Server running on port 5000!!'));
